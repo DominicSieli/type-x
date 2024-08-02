7edc0ca846c4da522d998fc0a5dfe57b0dbbcf13
DominicSieli <dominicsieli@gmail.com>
2024-08-02 03:43:36 -0700

    update project

--- script.js
+++ script.js
@@ -1,130 +1,130 @@
 const INPUT = document.querySelector('body');
-const INCORRECT_COUNTER = document.getElementById('incorrect-counter');
+const ERROR_COUNTER = document.getElementById('error-counter');
 const CHARACTER = document.getElementById('character');
 const ORDER_OPTIONS = document.getElementById("order-options");
 const CHARACTER_SETS = document.getElementById("character-sets");
 
 let index = 0;
 let timer = new Date();
-let incorrectCounter = 0;
+let errorCounter = 0;
 let characterOrder = "Sequential";
 
 const orderOptions = new Array(
-    "Sequential",
-    "Random"
+	"Sequential",
+	"Random"
 );
 
 const characterSets = new Map([
-    ["Digits", "0123456789"],
-    ["Bracket", "[]{}()<>"],
-    ["Puncuators", `_:;!?,."'`],
-    ["Operators", "~@#$%^&/*-+=|"],
-    ["Letters-Upper", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
-    ["Letters-Lower", "abcdefghijklmnopqrstuvwxyz"]
+	["Digits", "0123456789"],
+	["Bracket", "[]{}()<>"],
+	["Puncuators", `_:;!?,."'`],
+	["Operators", "~@#$%^&*-+=|\\/"],
+	["Letters-Upper", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+	["Letters-Lower", "abcdefghijklmnopqrstuvwxyz"]
 ]);
 
 let characters = characterSets.get(Array.from(characterSets.keys())[0]).split('');
 
-INCORRECT_COUNTER.textContent = incorrectCounter;
+ERROR_COUNTER.textContent = errorCounter;
 CHARACTER.textContent = characters[resetIndex()];
 INPUT.addEventListener('keyup', (input) => {isCorrect(input.key.charAt())});
 
 function isCorrect(character)
 {
-    if(character == characters[index]) CHARACTER.textContent = characters[getNextIndex()];
-    else if(character != characters[index]) INCORRECT_COUNTER.textContent = ++incorrectCounter;
+	if(character == characters[index]) CHARACTER.textContent = characters[getNextIndex()];
+	else if(character != characters[index]) ERROR_COUNTER.textContent = ++errorCounter;
 }
 
-function populateCharacters()
+function populateCharacterSets()
 {
-    for(let i = 0; i < characterSets.size; i++)
-    {
-        let opt = Array.from(characterSets.keys())[i];
-        let el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        CHARACTER_SETS.appendChild(el);
-    }
+	for(let i = 0; i < characterSets.size; i++)
+	{
+		let option = Array.from(characterSets.keys())[i];
+		let element = document.createElement("option");
+		element.textContent = option;
+		element.value = option;
+		CHARACTER_SETS.appendChild(element);
+	}
 }
 
 function populateOrderOptions()
 {
-    for(let i = 0; i < orderOptions.length; i++)
-    {
-        let opt = orderOptions[i];
-        let el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        ORDER_OPTIONS.appendChild(el);
-    }
+	for(let i = 0; i < orderOptions.length; i++)
+	{
+		let option = orderOptions[i];
+		let element = document.createElement("option");
+		element.textContent = option;
+		element.value = option;
+		ORDER_OPTIONS.appendChild(element);
+	}
 }
 
-populateCharacters();
+populateCharacterSets();
 populateOrderOptions();
 
 function reset()
 {
-    resetIndex();
-    incorrectCounter = 0;
-    INCORRECT_COUNTER.textContent = incorrectCounter;
-    CHARACTER.textContent = characters[index];
-    CHARACTER_SETS.blur();
-    INPUT.focus();
+	resetIndex();
+	errorCounter = 0;
+	ERROR_COUNTER.textContent = errorCounter;
+	CHARACTER.textContent = characters[index];
+	CHARACTER_SETS.blur();
+	INPUT.focus();
 }
 
 function resetIndex()
 {
-    if(characterOrder == "Sequential") index = 0;
-    else if(characterOrder == "Random") index = getRandomIndex();
+	if(characterOrder == "Sequential") index = 0;
+	else if(characterOrder == "Random") index = getRandomIndex();
 
-    return index;
+	return index;
 }
 
 function getNextIndex()
 {
-    if(characterOrder == "Sequential")
-    {
-        if(index < characters.length - 1) index += 1;
-        else index = 0;
-    }
-    else if(characterOrder == "Random") index = getRandomIndex();
+	if(characterOrder == "Sequential")
+	{
+		if(index < characters.length - 1) index += 1;
+		else index = 0;
+	}
+	else if(characterOrder == "Random") index = getRandomIndex();
 
-    return index;
+	return index;
 }
 
 function getRandomIndex()
 {
-    return Math.floor(Math.random() * (characters.length));
+	return Math.floor(Math.random() * (characters.length));
 }
 
-function changeOrder()
+function setOrder()
 {
-    ORDER_OPTIONS.value = ORDER_OPTIONS.options[ORDER_OPTIONS.selectedIndex].text;
-    characterOrder = ORDER_OPTIONS.options[ORDER_OPTIONS.selectedIndex].value;
-    ORDER_OPTIONS.blur();
-    INPUT.focus();
-    reset();
+	ORDER_OPTIONS.value = ORDER_OPTIONS.options[ORDER_OPTIONS.selectedIndex].text;
+	characterOrder = ORDER_OPTIONS.options[ORDER_OPTIONS.selectedIndex].value;
+	ORDER_OPTIONS.blur();
+	INPUT.focus();
+	reset();
 }
 
-function selectSet()
+function setCharacterSet()
 {
-    CHARACTER_SETS.value = CHARACTER_SETS.options[CHARACTER_SETS.selectedIndex].text;
-    characters = characterSets.get(CHARACTER_SETS.options[CHARACTER_SETS.selectedIndex].value).split('');
-    CHARACTER.textContent = characters[0];
-    CHARACTER_SETS.blur();
-    INPUT.focus();
-    reset();
+	CHARACTER_SETS.value = CHARACTER_SETS.options[CHARACTER_SETS.selectedIndex].text;
+	characters = characterSets.get(CHARACTER_SETS.options[CHARACTER_SETS.selectedIndex].value).split('');
+	CHARACTER.textContent = characters[0];
+	CHARACTER_SETS.blur();
+	INPUT.focus();
+	reset();
 }
 
 // function startTimer()
 // {
-//     timer = new Date();
+//	   timer = new Date();
 //
-//     setInterval(() => {
-//     }, 1000)
+//	   setInterval(() => {
+//	   }, 1000)
 // }
 //
 // function stopTimer()
 // {
-//     return Math.floor(new Date() - timer);
+//	   return Math.floor(new Date() - timer);
 // }
